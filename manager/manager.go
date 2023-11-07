fa6d216d85fd3a28b99c72cc4f216ac3bec7e2a7
Greatness <akumagreatness@gmail.com>
2023-11-08 00:21:25 +0100

    created manager object and it required methods

---
+++ manager/manager.go
@@ -0,0 +1,27 @@
+package manager
+
+import (
+	"fmt"
+
+	"github.com/golang-collections/collections/queue"
+	"github.com/google/uuid"
+	"cube/task"
+)
+
+type Manager struct {
+	Pending queue.Queue
+	TaskDb map[string][]Task
+	EventDb map[string][]TaskEvent
+	Workers []string
+	WorkersTaskMap map[string][]uuid.UUID
+	TaskWorkerMap map[uuid.UUID]string
+}
+func (m *Manager) SelectWorker(){
+	fmt.Println("Will select an appropriate worker")
+}
+func (m *Manager) UpdateTasks(){
+	fmt.Println("Will update task")
+}
+func (m *Manager) SendWork() {
+	fmt.Println("Will send work to workers")
+}
