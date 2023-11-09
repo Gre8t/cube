1b8021a0b51616eab0fa56eead932d5ad7bde2ee
Greatness <akumagreatness@gmail.com>
2023-11-09 14:57:55 +0100

    used the right import type for the orchestrator manager

--- manager/manager.go
+++ manager/manager.go
@@ -10,8 +10,8 @@ import (
 
 type Manager struct {
 	Pending queue.Queue
-	TaskDb map[string][]Task
-	EventDb map[string][]TaskEvent
+	TaskDb map[string][]task.Task
+	EventDb map[string][]task.TaskEvent
 	Workers []string
 	WorkersTaskMap map[string][]uuid.UUID
 	TaskWorkerMap map[uuid.UUID]string
